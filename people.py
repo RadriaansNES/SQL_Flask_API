2314fb79892492248205261739abd3c2617a8f1c
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-05-17 12:25:39 -0400

    REST API flask project v1 complete.

--- people.py
+++ people.py
@@ -1,5 +1,6 @@
 # Return data from people endpoint
 from datetime import datetime
+from flask import abort, make_response
 
 def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
@@ -24,3 +25,53 @@ PEOPLE = {
 
 def read_all():
     return list(PEOPLE.values())
+
+# using flasks abort function to raise error when the request body doesn't contain a lalst name/person already exists
+def create(person):
+    lname = person.get("lname")
+    fname = person.get("fname", "")
+
+    if lname and lname not in PEOPLE:
+        PEOPLE[lname] = {
+            "lname": lname,
+            "fname": fname,
+            "timestamp": get_timestamp(),
+        }
+        return PEOPLE[lname], 201
+    else:
+        abort(
+            406,
+            f"Person with last name {lname} already exists",
+        )
+# function to read one person 
+def read_one(lname):
+    if lname in PEOPLE:
+        return PEOPLE[lname]
+    else:
+        abort(
+            404, f"Person with last name {lname} not found"
+        )
+# update fuction
+def update(lname, person):
+    if lname in PEOPLE:
+        PEOPLE[lname]["fname"] = person.get("fname", PEOPLE[lname]["fname"])
+        PEOPLE[lname]["timestamp"] = get_timestamp()
+        return PEOPLE[lname]
+    else:
+        abort(
+            404,
+            f"Person with last name {lname} not found"
+        )
+
+# delete function - check if data there, and if it is, wipe
+def delete(lname):
+    if lname in PEOPLE:
+        del PEOPLE[lname]
+        return make_response(
+            f"{lname} successfully deleted", 200
+        )
+    else:
+        abort(
+            404,
+            f"Person with last name {lname} not found"
+        )
